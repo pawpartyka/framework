7a66aa2cd95601749f8e00d24a55b6a43832dd50
Paweł Partyka <pawel.partyka@nutreek.com>
2020-06-21 17:53:11 +0200

    cli

--- packages/cli/src/schematics/application/index.ts
+++ packages/cli/src/schematics/application/index.ts
@@ -1,15 +1,12 @@
 import { normalize, strings } from '@angular-devkit/core';
-import { apply, applyTemplates, mergeWith, move, url, MergeStrategy, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { join, parse } from 'path';
+import { apply, applyTemplates, mergeWith, move, url, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { parse } from 'path';
 import { ApplicationSchema } from './schema';
 
 export function application(options: ApplicationSchema): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const name: string = parse(options.name).name;
     const path: string = normalize(strings.dasherize(options.name));
-
-    context.addTask(new NodePackageInstallTask());
 
     return mergeWith(apply(url('./files'), [
       applyTemplates({ ...strings, name: name }),
