1c8f15080c796c6dd12288f2ac479ed371046c68
Paweł Partyka <partyka95@icloud.com>
2020-08-09 18:13:58 +0200

    fix get provider name helper

--- packages/core/src/helpers/get-provider-name.helper.ts
+++ packages/core/src/helpers/get-provider-name.helper.ts
@@ -1,6 +1,7 @@
 import { Provider } from '../interfaces/provider.interface';
+import { getTokenName } from './get-token-name.helper';
 import { isTypeProvider } from './is-type-provider.helper';
 
 export function getProviderName(provider: Provider): string {
-  return isTypeProvider(provider) ? provider.name : this.getTokenName(provider.provide);
+  return isTypeProvider(provider) ? provider.name : getTokenName(provider.provide);
 }
