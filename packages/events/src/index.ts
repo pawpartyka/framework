c08840da18216c2ec3fc333649e223a80bc744f7
Paweł Partyka <partyka95@icloud.com>
2020-11-02 20:54:47 +0100

    chore: remove events package

--- packages/events/src/index.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata';
-
-export * from './decorators/on-event.decorator';
-export * from './services/emitter.service';
-export * from './services/registry.service';
-export * from './events.manager';
-export * from './events.package';
--- packages/events/src/services/registry.service.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@artisanjs/core';
-
-@Injectable()
-export class Registry extends Array<{ event: string, handler: (...args: any[]) => any }> {
-}
