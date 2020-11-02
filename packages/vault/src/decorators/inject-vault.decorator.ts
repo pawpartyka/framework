3308d77a6cdaa62f283e62979d07866fdf9930dc
Paweł Partyka <partyka95@icloud.com>
2020-11-02 20:53:55 +0100

    chore: remove vault package

--- packages/vault/src/decorators/inject-vault.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { Inject } from '@artisanjs/core';
-import { getVaultToken } from '../providers/vault.provider';
-
-export function InjectVault(name: string): ParameterDecorator {
-  return Inject(getVaultToken(name));
-}
--- packages/vault/src/interfaces/vault-options.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface VaultOptions {
-  namespace?: string; // X-Vault-Namespace:
-  token?: string; // X-Vault-Token:
-  url?: string;
-  // X-Vault-Request: true
-}
