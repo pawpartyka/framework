32ba24983b5822ae2c1f0c27c615b6f7cff10f26
Paweł Partyka <partyka95@icloud.com>
2021-04-06 22:34:01 +0200

    remove state-machine package

--- packages/state-machine/src/lib/decorators/inject-state-machine.ts
+++
@@ -1,6 +0,0 @@
-import { Inject } from '@artisanjs/core';
-import { getStateMachineToken } from '../providers/state-machine';
-
-export function InjectStateMachine(name: string): ParameterDecorator {
-  return Inject(getStateMachineToken(name));
-}
--- packages/state-machine/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-
-export * from './lib/decorators/inject-state-machine';
-export * from './lib/providers/state-machine';
-export * from './lib/providers/state-machine-options';
-export * from './lib/state-machine-package';
